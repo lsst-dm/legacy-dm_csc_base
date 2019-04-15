3e940a4d877d23fe33367eac448dcf0155086aa3
Stephen R Pietrowicz <srp@illinois.edu>
2019-04-15 15:43:49 -0500

    revert change.  This will be dealt with in DM-19278

--- Scoreboard.py
+++ Scoreboard.py
@@ -33,7 +33,7 @@ from lsst.ctrl.iip.iip_base import iip_base
 
 LOGGER = logging.getLogger(__name__)
 
-class Scoreboard():
+class Scoreboard(iip_base):
     """This is the parent class of the three scoreboard classes. 
        It, and they, form an interface for the Redis in-memory DB
        that continually stores state information about components and jobs.
