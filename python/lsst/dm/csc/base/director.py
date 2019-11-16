ffa4f5c534f1aeac45a53d98afa039be11beac73
Stephen R Pietrowicz <srp@illinois.edu>
2019-11-16 12:57:24 -0600

    Add support for Event object map

--- python/lsst/dm/csc/base/director.py
+++ python/lsst/dm/csc/base/director.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import asyncio
 import datetime
 import logging
 from lsst.dm.csc.base.base import base
@@ -45,6 +46,40 @@ class Director(base):
         url = f"amqp://{service_user}:{service_passwd}@{self.base_broker_addr}"
 
         self.base_broker_url = url
+
+        self._ack_lock = asyncio.Lock()
+        self._ack_id = 0
+        self._event_map_lock = asyncio.Lock()
+        self._event_map = {}
+
+    async def create_event(self, ack_id):
+        evt = asyncio.Event()
+        with await self._event_map_lock:
+            self._event_map[ack_id] = evt
+        return evt
+
+    async def clear_event(self, ack_id):
+        evt = await self.retrieve_event(ack_id)
+        if evt is None:
+            LOGGER.info(f"Event does not exist: {ack_id}.")
+        else:
+            evt.clear()
+        return evt
+        
+    async def retrieve_event(self, ack_id):
+        evt = None
+        with await self._event_map_lock:
+            if ack_id in self._event_map:
+                evt = self._event_map.pop(ack_id)
+        return evt
+
+    async def get_next_ack_id(self):
+        ack_id_val = 0
+        with await self._ack_lock:
+            self._ack_id += 1
+            ack_id_val = self._ack_id
+        ack_id = f"{self.session_id}_{ack_id_val}"
+        return ack_id
 
     def initialize_session(self):
         self.session_id = str(datetime.datetime.now()).replace(' ','_')
